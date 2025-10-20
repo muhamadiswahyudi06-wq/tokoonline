244b8c42af96a63dc0264de282e230b077190b6f
kal2301 <muhammadhaikalhizriyanachmyzad@gmail.com>
2025-10-20 13:46:53 +0700

    hadir  haikal

--- adminpanel/Footer.php
+++
@@ -1,7 +0,0 @@
-    </main>
-    <footer>
-        <p>&copy; 2023 Cokelat Elegan. Semua hak cipta dilindungi.</p>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
